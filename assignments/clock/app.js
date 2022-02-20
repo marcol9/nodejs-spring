2833a3741ec50f8aeebf9811f7affc431a80a78c
marcol9 <marekslipnickis@gmail.com>
2022-02-20 20:28:55 +0100

    commit name

---
+++ assignments/clock/app.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/public/clock.html");
+});
+app.use(express.static(__dirname + "/public"));
+
+app.listen(process.env.PORT || "5000");
